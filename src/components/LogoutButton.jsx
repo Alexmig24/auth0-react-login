13a7bc553a7876d0e97f90e8aefef9725d617258
Alexmig24 <alexandermqc2004@hotmail.com>
2026-01-12 16:08:29 -0500

    Create component the button for logout

---
+++ src/components/LogoutButton.jsx
@@ -0,0 +1,13 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+const LoginButton = () => {
+    const { logout } = useAuth0();
+    return <button
+        onClick={()=>logout({ returnTo: window.location.origin })}
+        className="logout button"
+    > 
+        Log Out 
+    </button>
+}
+
+export default LoginButton;
